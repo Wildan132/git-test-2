9461658d5b30f8a53d747c36c11dc2117b040a8d
Wildan132 <mwp113322758@gmail.com>
2022-11-09 18:29:03 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,11 @@
 <body>
     <h1>Hello world!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim tenetur omnis excepturi repellendus adipisci nisi est recusandae accusantium, facere earum ullam quod molestiae aperiam, assumenda atque, magnam minima possimus doloremque?</p>
+
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
 </body>
 </html>
